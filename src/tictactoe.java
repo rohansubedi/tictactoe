3873ca921d53e18f474b8177889c0373e35e76a4
Rohan Subedi <rsubedi@bruinmail.slcc.edu>
2018-06-21 19:32:10 -0600

    Added support to take in player names

---
+++ src/tictactoe.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class tictactoe {
+
+
+    public static void main(String[] args){
+        String name1;
+        String name2;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Player 1 name: ");
+        name1 = input.nextLine();
+
+        System.out.println("Player 2 name: ");
+        name2 = input.nextLine();
+
+    }
+}
