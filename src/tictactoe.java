a354bda11d58aa72fc8e9e24b94fa6b606ef24a5
Rohan Subedi <rsubedi@bruinmail.slcc.edu>
2018-06-21 19:40:18 -0600

    Created the board and a dummy loop

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -2,16 +2,32 @@ import java.util.*;
 public class tictactoe {
 
 
+   // public static gameUver{
+
+    //}
+
     public static void main(String[] args){
         String name1;
         String name2;
         Scanner input = new Scanner(System.in);
-
+        boolean gameOver = false;
         System.out.println("Player 1 name: ");
         name1 = input.nextLine();
 
         System.out.println("Player 2 name: ");
         name2 = input.nextLine();
 
+        int board[] = new int[9];
+        System.out.println("___|___|___");
+        System.out.println("   |   |   ");
+        System.out.println("___|___|___");
+        System.out.println("   |   |   ");
+
+        while(gameOver != true){
+            System.out.println("try again");
+            System.out.println("try again");
+
+        }
     }
+
 }
