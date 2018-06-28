5796918566078a876e8015bcbcbf6aa67a6c3249
Rohan Subedi <rsubedi@bruinmail.slcc.edu>
2018-06-27 22:01:29 -0600

    player 1 and player 2 common entries are ignored

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -8,7 +8,7 @@ public class tictactoe {
         boolean gameOver = false;
         int x;
         int o;
-        int [] n = new int [3];
+        int[] n = new int[3];
 
 
         System.out.println("Player 1 name: ");
@@ -17,32 +17,58 @@ public class tictactoe {
         System.out.println("Player 2 name: ");
         name2 = input.nextLine();
 
-        int board[] = new int[9];
+        int board[] = new int[100];
 
 
         while (gameOver != true) {
-            System.out.println(name1 + "'s turn!");
+            System.out.println(name1 + "'s turn! Choose a number from 0 - 8");
             x = input.nextInt();
-            
-            while(board[x] != 0 ){
-                System.out.println("Invalid. Please try again!");
+
+            while (!(x <= 8)) {
+                System.out.println("Invalid, Please try again!");
                 x = input.nextInt();
+                while (board[x] != 0) {
+                    System.out.println("Invalid. Please try again!");
+                    x = input.nextInt();
 
+                }
+                board[x] = 1;
             }
-            board[x] = 1;
 
 
-
-            System.out.println(name2 + "'s turn!");
+            System.out.println(name2 + "'s turn! Choose a number from 0 - 8");
             o = input.nextInt();
-            while(board[o] != 0 && board[o] >= 9){
-                System.out.println("Invalid. Please try again!");
+            while (!(o <= 8)) {
+                System.out.println("Invalid location! try again!");
+                o = input.nextInt();}
+                while(o == x ){
+                System.out.println("Location already taken! please try again!");
                 o = input.nextInt();
 
+
+
+                    while (board[o] != 0) {
+                        System.out.println("Invalid. Please try again!");
+                        o = input.nextInt();
+                    }
+                }
+                board[o] = 2;
+
+            System.out.println(name1 + "'s turn! Choose a number from 0 - 8");
+            x = input.nextInt();
+
+            while (!(x <= 8)) {
+                System.out.println("Invalid, Please try again!");
+                x = input.nextInt();}
+                while(x == o){
+                    System.out.println("Location already taken! Please try again!");
+                    while (board[x] != 0) {
+                        System.out.println("Invalid. Please try again!");
+                        x = input.nextInt();
+                    }
+                }
+                board[x] = 1;
             }
-            board [o] = 2;
-
-            System.out.println(Arrays.toString(board));
 
 
 
@@ -50,13 +76,18 @@ public class tictactoe {
 
 
 
-            }
+
+
+
             //if(board[x]
 
             //gameOver = true;
 
         }
-
-
     }
+
+
+
+
+
 
